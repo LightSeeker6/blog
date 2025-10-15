1ad5e92d2b697ce2ec50e069b3828bcc372e2441
yolo <3522373084@qq.com>
2025-10-15 18:02:12 +0800

    修改内容为自己的

--- main.py
+++ main.py
@@ -9,18 +9,16 @@ from github import Github
 from lxml.etree import CDATA
 from marko.ext.gfm import gfm as marko
 
-MD_HEAD = """**<p align="center">[Leeyom's Blog](https://blog.leeyom.top)</p>**
+MD_HEAD = """**<p align="center">[LightSeeker's Blog]()</p>**
 ====
 
-**<p align="center">用于记录一些幼稚的想法和脑残的瞬间</p>**
-[![](https://raw.githubusercontent.com/superleeyom/blog/main/img/IMG_216.JPEG)](https://blog.leeyom.top)
+**<p align="center">用于记录</p>**
 
 ## 联系方式
-- Twitter：[@super_leeyom](https://twitter.com/super_leeyom)
-- Telegram：[@super_leeyom](https://t.me/super_leeyom)
-- Email：[leeyomwang@163.com](mailto:leeyomwang@163.com)
-- Blog：[https://blog.leeyom.top](https://blog.leeyom.top)
-- RSS：[RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
+- Twitter：待定
+- Telegram：待定
+- Email：待定
+- Blog：待定
 """
 
 BACKUP_DIR = "backup"
